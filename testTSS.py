b117ac86aeb5a6aacc9d3395ae47b72263b3851e
brandhsn <sebastian.brandhofer@gmail.com>
2017-01-11 02:15:53 +0100

    added un_registerKey and use case

--- testTSS.py
+++ testTSS.py
@@ -6,11 +6,20 @@ import uuid
 from pytss.tspi_defines import *
 import binascii
 
+import os.path
+
+
 #from tspi_defines import *
 #import interface
 #from interface import tss_lib
 #help(pytss)
 srk_uuid = uuid.UUID('{00000000-0000-0000-0000-000000000001}')
+old_uuid = uuid.UUID('{10000000-0000-0000-0000-000000000001}')
+new_uuid = uuid.UUID('{20000000-0000-0000-0000-000000000001}')
+masterKeyFilePath= "masterkey"
+
+keyFlags =  TSS_KEY_TYPE_BIND | TSS_KEY_SIZE_2048 | TSS_KEY_NO_AUTHORIZATION | TSS_KEY_NOT_MIGRATABLE
+
 srkSecret = bytearray([0] * 20)
 def take_ownership(context):
     """Take ownership of a TPM
@@ -32,118 +41,54 @@ def take_ownership(context):
 
     return True
 
-def exchangeMKWrap(oldKeyBinFile,dataList):
-    newData=exchangeMasterKey(oldKeyBinFile,dataList)
-
-    with open('newKey.bin', 'rb') as f:
-        newkblob=bytearray(f.read())
-
-    #exchange data with newData atomically, possible? or some critical section flag which is read on a crash restart
-    currentData=newData
-    with open(oldKeyBinFile, 'wb') as f:
-        f.write(newkblob)
-    return currentData
-
-def exchangeMasterKey(oldKeyBinFile,dataList):
-    context=TspiContext()
-    context.connect()
-
-    take_ownership(context)
-    srk = context.load_key_by_uuid(TSS_PS_TYPE_SYSTEM, srk_uuid)
-
+def getSrkKey(context):
+    srk= context.load_key_by_uuid(TSS_PS_TYPE_SYSTEM, srk_uuid)
     srkpolicy = srk.get_policy_object(TSS_POLICY_USAGE)
     srkpolicy.set_secret(TSS_SECRET_MODE_SHA1, srkSecret)
+    return srk
+def getMasterkeyNumberArray():
+    #return "MASTERKEY"
+    return [77, 65, 83, 84, 69, 82, 75, 69, 89]
+def clearKeys():
+    try:
+        k1 = context.load_key_by_uuid(tss_lib.TSS_PS_TYPE_SYSTEM,old_uuid)
+        k1.unregisterKey()
+        k2 = context.load_key_by_uuid(tss_lib.TSS_PS_TYPE_SYSTEM,new_uuid)
+        k2.unregisterKey()
+    except:
+        pass
 
-
-    flags =  TSS_KEY_TYPE_BIND | TSS_KEY_SIZE_2048 | TSS_KEY_NO_AUTHORIZATION | TSS_KEY_NOT_MIGRATABLE
-    k=context.create_wrap_key(flags,srk.get_handle())
-    k.load_key()
-    with open('newKey.bin', 'wb') as f:
-        f.write(k.get_keyblob())
-
-    with open(oldKeyBinFile, 'rb') as f:
-     oldkblob= bytearray(f.read())
-
-    oldk=context.load_key_by_blob(srk, oldkblob)
-
-    newDataList=[]
-    for data in dataList:
-        #TODO: load keys at each bind/unbind?
-        #oldk=context.load_key_by_blob(srk, oldkblob)
-
-        tmpClearData=oldk.unbind(data)
-        print("clearData:"+str(tmpClearData))
-        #k.load_key()
-        newDataList.append(k.bind(tmpClearData))
-
-    #with open(oldKeyBinFile, 'wb') as f:
-        #f.write(k.get_keyblob())
-    print("ko:"+binascii.hexlify(oldkblob))
-    print("kn:"+binascii.hexlify(k.get_keyblob()))
-    return newDataList
-print("hi")
 context=TspiContext()
 context.connect()
 
 take_ownership(context)
-srk = context.load_key_by_uuid(TSS_PS_TYPE_SYSTEM, srk_uuid)
+srk = getSrkKey(context)
 
-srkpolicy = srk.get_policy_object(TSS_POLICY_USAGE)
-srkpolicy.set_secret(TSS_SECRET_MODE_SHA1, srkSecret)
+#clearKeys()
 
-
-flags =  TSS_KEY_TYPE_BIND | TSS_KEY_SIZE_2048 | TSS_KEY_NO_AUTHORIZATION | TSS_KEY_NOT_MIGRATABLE
-#hand= ffi.new(ctype)
-#tss_lib.Tspi_Context_CreateObject(context, tss_type, flags, hand)
-k=context.create_wrap_key(flags,srk.get_handle())
-
-print("handle:"+str(k.get_handle()))
-
-
-#kk=context.load_key_by_blob(srk, k.get_keyblob())
-k.load_key()
-dat=[0x61,0x62,0x63]
-encdat = k.bind(dat)
-#tss_lib.Tspi_Data_Bind()
-print("encry:"+encdat)
-
-decdat =k.unbind(encdat)
-print("decry:"+decdat)
-
-with open('somefile.bin', 'wb') as f:
-    f.write(k.get_keyblob())
-
-newk = None
-with open('somefile.bin', 'rb') as f:
-    newk= bytearray(f.read())
-#print("k:"+binascii.hexlify(k.get_keyblob()))
-#print("k:"+binascii.hexlify(newk))
-
-kk=context.load_key_by_blob(srk, newk)
-encdat = kk.bind(dat)
-#tss_lib.Tspi_Data_Bind()
-print("encry:"+encdat)
-
-decdat =kk.unbind(encdat)
-print("decrysds:"+decdat)
-
-dataL=[kk.bind([0x61]),kk.bind([0x62]),kk.bind([0x63])]
-print("dataL"+str(dataL))
-dataL=exchangeMKWrap('somefile.bin',dataL)
-print("dataL"+str(dataL))
-print("=============================================1")
-dataL=exchangeMKWrap('somefile.bin',dataL)
-print("dataL"+str(dataL))
-print("=============================================2")
-dataL=exchangeMKWrap('somefile.bin',dataL)
-print("dataL"+str(dataL))
-print("=============================================3")
-
-
-#print cap
-#pytss.interface.Tspi_TPM_GetStatus(blaa.get_tpm_object(),1,1)
-#print str(blaa.get_tpm_object())
-#print dir(pytss.interface)
-#print "blaa:"+str(pytss.interface.Tspi_TPM_GetStatus())
-## 
-#print dir(pytss)
+MKencrypted=True
+if not os.path.isfile(masterKeyFilePath):
+    MKencrypted=False
+    
+if MKencrypted:
+    #exchange keys: assumption old_uuid key exists, new_uuid key does not exist. oldkey encrypted masterkeyFile
+    kOld=context.load_key_by_uuid(tss_lib.TSS_PS_TYPE_SYSTEM,old_uuid)
+    kNew=context.create_wrap_key(keyFlags,srk.get_handle())
+    with open(masterKeyFilePath, 'rb+') as f:
+        encryptedData = bytearray(f.read())
+        f.seek(0)
+        #print("data: "+kOld.unbind(encryptedData))
+        kNew.registerKey(new_uuid,srk_uuid)
+        f.write(kNew.bind(kOld.unbind(encryptedData)))
+        f.truncate()
+    kOld.unregisterKey()
+    kNew.registerKey(old_uuid,srk_uuid)
+    kNew.unregisterKey(new_uuid)
+    #kNew.loadkey if you want to use it further
+else:
+    #encrypt and save
+    k=context.create_wrap_key(keyFlags,srk.get_handle())
+    k.load_key()
+    k.registerKey(old_uuid,srk_uuid)
+    with open(masterKeyFilePath, 'wb') as f:
+        f.write(k.bind(getMasterkeyNumberArray()))
